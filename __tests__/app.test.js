473b1276c0780c7f4c4242159962a34bba681b3f
kathrynzaro <zarokathryn@gmail.com>
2022-07-28 16:17:58 -0700

    adding styles and passing tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,13 +3,35 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+describe('charts routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('/cats should return a list of charts', async () => {
+    const res = await request(app).get('/charts');
+    expect(res.body.length).toEqual(7);
+    expect(res.body[6]).toEqual({
+      id: '7',
+      name: 'Kashi',
+    });
   });
+
+  it('/charts/:id should return chart detail', async () => {
+    const res = await request(app).get('/charts/1');
+    const kat = {
+      id: '1',
+      name: 'Kat',
+      age: 28,
+      birthday: 'July 7, 1994',
+      sun: 'Cancer',
+      moon: 'Cancer',
+      rising: 'Leo',
+      venus: 'Leo',
+    };
+    expect(res.body).toEqual(kat);
+  });
+
   afterAll(() => {
     pool.end();
   });
